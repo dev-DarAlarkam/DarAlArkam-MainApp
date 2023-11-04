c910ba52afa86ba2f7907b42e362d5a79f141c47
Yossef Khader <117017467+yossefkhader@users.noreply.github.com>
2023-11-04 12:58:09 +0200

    Activities third commit

--- lib/ui/activities/ActivitiesList.dart
+++ lib/ui/activities/ActivitiesList.dart
@@ -1,7 +1,10 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:daralarkam_main_app/backend/Activities/Activity.dart';
 import 'package:daralarkam_main_app/backend/classReport/classReport.dart';
+import 'package:daralarkam_main_app/ui/activities/ActivityViewerWidgets.dart';
 import 'package:daralarkam_main_app/ui/activities/activityViewer.dart';
+import 'package:daralarkam_main_app/ui/widgets/text.dart';
 import 'package:flutter/material.dart';
 
 // Enum to determine the sort order for students' list
@@ -66,7 +69,6 @@ class _ActivitiesListTabState extends State<ActivitiesListTab> {
             stream: readEvents(),
             builder: (context, snapshot) {
               if (snapshot.hasError) {
-                print("hi)");
                 return Text(snapshot.error.toString());
               } else if (snapshot.hasData) {
                 events = snapshot.data as List<Activity>;
@@ -88,18 +90,73 @@ class _ActivitiesListTabState extends State<ActivitiesListTab> {
 
 
   Widget buildEvent(Activity activity){
-    return Stack(
-      children: [
-        Positioned()
-      ],
-    )
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => ActivityViewer(activity: activity,)));
+            },
+            child: Container(
+              height: height * 0.2,
+              width: width *0.9,
+              decoration: BoxDecoration(
+                // border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 3,
+                  blurRadius: 7,
+                  offset: const Offset(0, 6)
+                )]
+              ),
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      width: width*0.9,
+                      height: height*0.2,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: buildThumbnail(activity.thumbnail),
+                      clipBehavior: Clip.antiAlias,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    height: height * 0.1,
+                    width: width*0.9,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.95), borderRadius: BorderRadius.circular(20)),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        boldColoredArabicText(activity.title),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(activity.date),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),        
+        ],
+      ),
+    );
   }
-
-//   // Build a list tile for a student
-//   Widget buildEvent(Activity activity) => ListTile(
-//     title: Text(activity.title),
-//     onTap: () {
-//       Navigator.push(context, MaterialPageRoute(builder: (context)=> ActivityViewer(activity: activity)));
-//     },
-//   );
 }
